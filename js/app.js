0342054f765520f89bb07e32b30901aa23183af2
Stephen Schroeder <schrode50@gmail.com>
2016-02-02 21:54:43 -0800

    clean up

--- js/app.js
+++ js/app.js
@@ -17,48 +17,6 @@ var rawData = [
   }
 ];
 
-// function Jobs(opts) {
-//   this.name = opts.name;
-//   this.published = opts.date;
-//   this.image = opts.image;
-//   this.text = opts.text;
-//   this.link = opts.link;
-// };
-//
-// Jobs.prototype.toHtml = function() {
-//   var $newJob = $('article.template').clone();
-//   // $newJob.find('by-line a').attr('href', this.authorUrl);
-//   $newJob.attr('data-category', this.category);
-//   $newJob.find('h2').text(this.title);
-//   $newJob.find('.desc').html(this.text);
-//   $newJob.find('time').html(this.publishedOn);
-//
-//   $newJob.removeClass('template');
-//   $newJob.append(this.body);
-//
-//
-//   $newJob.find('time[pubdate]').attr('title', this.publishedOn);
-//
-//   $newJob.find('time').html('about ' + parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000) + ' days ago');
-//
-//   $newJob.append('<hr>');
-//
-//   $newJob.removeClass('template');
-//
-//   return $newJob;
-// };
-//
-// rawData.sort(function(a,b) {
-//   return(new Date(b.publishedOn)) - (new Date(a.publishedOn));
-// });
-//
-// rawData.forEach(function(ele) {
-//   allJobs.push(new Jobs(ele));
-// });
-//
-// allJobs.forEach(function(a) {
-//   $('#portList').append(a.toHtml());
-// });
 function dateSort(){
   console.log('hit dateSort');
   rawData.sort(function(a,b){
@@ -88,22 +46,6 @@ Work.prototype.toHtml = function(obj){
   $('#portfolio').append($newItem);
 
 };
-//
-// function pageWrite(){
-//   dateSort();
-//   console.log(rawData);
-//   for (var i = rawData.length - 1 ; i >= 0; i--){
-//     var $article = $('<li class="item"></li>');
-//     $article.append('<div><a href="' + rawData[i].link + '">'+ rawData[i].name +'</a></div>');
-//     $article.append('<div>'+ rawData[i].date +'</div>');
-//     $article.append('<div>'+ rawData[i].text +'</div>');
-//     $article.append('<img src="' + rawData[i].image + '">');
-//     console.log($article);
-//     $('#portfolio').append($article);
-//   }
-// }
-//
-// pageWrite();
 
 function writeItem(){
   dateSort();
