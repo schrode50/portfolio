d7ad690ed99a98c7de578be9232af921c2c50c5f
Stephen Schroeder <schrode50@gmail.com>
2016-02-02 23:59:26 -0800

    just trying to get thisin before 12

--- js/app.js
+++ js/app.js
@@ -18,11 +18,9 @@ var rawData = [
 ];
 
 function dateSort(){
-  console.log('hit dateSort');
   rawData.sort(function(a,b){
     var dateA = new Date(a.date);
     var dateB = new Date(b.date);
-
     return dateA - dateB;
   });
 }
@@ -33,26 +31,41 @@ var Work = function(input){
   this.image = input.image;
   this.text = input.text;
   this.link = input.link;
-  // rawData.push(this);
 };
 
 Work.prototype.toHtml = function(obj){
   var $newItem = $('li.template').clone();
   $('.template .title').html('<a href="' + obj.link + '">'+ obj.name +'</a>');
-  $('.template .date').html(obj.date);
+  $('.template .date').html('<h3>Published On: ' + obj.date);
   $('.template .pic').html('<img src="' + obj.image + '">');
   $('.template .desc').text(obj.text);
   $newItem.removeClass('template');
   $('#portfolio').append($newItem);
-
 };
 
-function writeItem(){
+function populate(){
   dateSort();
   for (var i = rawData.length - 1 ; i >= 0; i--){
     var item = new Work(rawData[i]);
     item.toHtml(item);
   }
 };
+populate();
 
-writeItem();
+// $('nav').on('click', function(event) {
+//   var $name = ($(event.target).data('name'));
+//   if($(event.target).hasClass('tab')) {
+//     $('.tab-content').hide();
+//     $('#' + $name).fadeIn('fast');
+//   }
+// });
+
+Work.handleNav = function() {
+  $('nav').on('click', function(event) {
+    var $tabContent = $('.tab');
+    var $dataContent = $(this).attr('data-content');
+    $('#full_list').hide();
+    $tabContent.filter('#' + $dataContent).show();
+  });
+};
+Work.handleNav();
