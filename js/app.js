2867b2c6b2b64ff10a19bbc2a9b7633992276ba6
Stephen Schroeder <schrode50@gmail.com>
2016-02-04 22:54:43 -0800

    handlebars done. responsive improvements made

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ var allJobs = [];
 var rawData = [
   {
     name:   'Cookie Stand',
-    date:   '2015-12-10',
+    // date:   '2015-12-10',
     image:  '../img/cookie.jpeg',
     text:   'Just a neat project for a store to sell Salmon Cookies. Not hosted',
     link:   'https://github.com/schrode50/cookie-stand'
@@ -17,14 +17,6 @@ var rawData = [
   }
 ];
 
-function dateSort(){
-  rawData.sort(function(a,b){
-    var dateA = new Date(a.date);
-    var dateB = new Date(b.date);
-    return dateA - dateB;
-  });
-}
-
 var Work = function(input){
   this.name = input.name;
   this.date = input.date;
@@ -34,64 +26,37 @@ var Work = function(input){
 };
 
 Work.prototype.toHtml = function(obj){
-  var $newItem = $('li.template').clone();
-  $('.template .title').html('<a href="' + obj.link + '">'+ obj.name +'</a>');
-  $('.template .date').html('<h3>Published On: ' + obj.date);
-  $('.template .pic').html('<img src="' + obj.image + '">');
-  $('.template .desc').text(obj.text);
-  $newItem.removeClass('template');
-  $('#portfolio').append($newItem);
+
+  var template = Handlebars.compile($('#article-template').text());
+
+  this.daysAgo = parseInt((new Date() - new Date(this.date))/60/60/24/1000);
+  this.publishStatus = this.date ? 'about ' + this.daysAgo + ' days old' : '(draft)';
+
+  return template(this);
 };
 
-function populate(){
-  dateSort();
-  for (var i = rawData.length - 1 ; i >= 0; i--){
-    var item = new Work(rawData[i]);
-    item.toHtml(item);
-  }
-};
-populate();
+rawData.sort(function(a,b) {
+  return (new Date(b.date)) - (new Date(a.date));
+});
 
-// $('.tab').on('click', function(event) {
-//   var $name = ($(event.target).data('name'));
-//   if($(event.target).hasClass('tab')) {
-//     $('.tab-content').hide();
-//     $('section[data]').fadeIn('fast');
-//     console.log(this);
-//     console.log($('#' + $name).fadeIn('fast'));
-//   }
-// });
+rawData.forEach(function(ele) {
+  allJobs.push(new Work(ele));
+});
 
-// Work.handleNav = function() {
-//   $('.tab').on('click', function(event) {
-//     var $tabContent = $('.tab');
-//     var $dataContent = $(this).attr('data-content');
-//     $('#full_list').hide();
-//     $('.tab[data-name = "'"#" + $(this).val()]).fadeIn();
-//     // $tabContent.filter('#' + $dataContent).show();
-//     $('this').show('#' + $dataContent);
-//     console.log(this);
-//   });
-// };
-// Work.handleNav();
+allJobs.forEach(function(a){
+  $('#portfolio').append(a.toHtml());
+});
 
 Work.handleNav = function() {
   $('.main-nav').on('click', '.tab', function(e) {
     $('#portfolio').hide();
-  var myID =  $('#' + $(this).data('content')).fadeIn();
-    // var ok=$(event.target).text().toLowerCase();
-    // var myID = $('#' + ok);
-    // $('#full_list').not(myID).hide();
-    // var close=$('#' + ok).fadeIn();
-
-    // $('#' + $(this).data('content')).fadeIn();
-    // $('#' + '.tab[data-content="' + $(this).val() + '"]').fadeIn();
-    // console.log(event.target);
-    console.log($('#' + $(this).data('content')).fadeIn());
+    $('#' + $(this).data('content')).fadeIn();
+    console.log();
     console.log(myID);
-    // console.log(close);
 
   });
-
 };
-Work.handleNav();
+
+$(document).ready(function() {
+  Work.handleNav();
+});
--- js/raw.js
+++ js/raw.js
@@ -1,9 +1,56 @@
-var rawData = [
-  {
-    name:   'BeerNuts',
-    date:   '27-12-2015',
-    image:  '../img/beer.jpg',
-    text:   'Beer information page with associated triva game',
-    link:   'http://laceylin2010.github.io/BeerNuts/'
-  }
-];
+// Work.populate = function(){
+//   dateSort();
+//   for (var i = rawData.length - 1 ; i >= 0; i--){
+//     var item = new Work(rawData[i]);
+//     item.toHtml(item);
+//   }
+// };
+
+// Work.populate();
+
+// function dateSort(){
+//   rawData.sort(function(a,b){
+//     var dateA = new Date(a.date);
+//     var dateB = new Date(b.date);
+//     return dateA - dateB;
+//   });
+// }
+// $('.tab').on('click', function(event) {
+//   var $name = ($(event.target).data('name'));
+//   if($(event.target).hasClass('tab')) {
+//     $('.tab-content').hide();
+//     $('section[data]').fadeIn('fast');
+//     console.log(this);
+//     console.log($('#' + $name).fadeIn('fast'));
+//   }
+// });
+
+// Work.handleNav = function() {
+//   $('.tab').on('click', function(event) {
+//     var $tabContent = $('.tab');
+//     var $dataContent = $(this).attr('data-content');
+//     $('#full_list').hide();
+//     $('.tab[data-name = "'"#" + $(this).val()]).fadeIn();
+//     // $tabContent.filter('#' + $dataContent).show();
+//     $('this').show('#' + $dataContent);
+//     console.log(this);
+//   });
+// };
+// Work.handleNav();
+
+// var $newItem = $('li.template').clone();
+// $('.template .title').html('<a href="' + obj.link + '">'+ obj.name +'</a>');
+// $('.template .date').html('<h3>Published On: ' + obj.date);
+// $('.template .pic').html('<img src="' + obj.image + '">');
+// $('.template .desc').text(obj.text);
+// $newItem.removeClass('template');
+// $('#portfolio').append($newItem);
+
+// var ok=$(event.target).text().toLowerCase();
+// var myID = $('#' + ok);
+// $('#full_list').not(myID).hide();
+// var close=$('#' + ok).fadeIn();
+
+// $('#' + $(this).data('content')).fadeIn();
+// $('#' + '.tab[data-content="' + $(this).val() + '"]').fadeIn();
+// console.log(event.target);
