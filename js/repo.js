36ccff0fa108047b3d4b71c3074c79b90229decb
Stephen Schroeder <schrode50@gmail.com>
2016-02-16 20:52:21 -0800

    pushing some work. not quite done. need retrieved data to display still

---
+++ js/repo.js
@@ -0,0 +1,27 @@
+(function(module) {
+  var repos = {};
+  repos.all = [];
+
+  repos.requestRepos = function(callback) {
+    var repoUrl = 'https://api.github.com/users/schrode50/repos';
+    var eTag;
+    $.ajax({
+      url: repoUrl + '?per_page=5&sort=updated',
+      type: 'GET',
+      headers: { 'Authorization': 'token ' + gitToken},
+      success: function(data, message, xhr) {
+        eTag = xhr.getResponseHeader('eTag');
+        console.log(data);
+        repos.all = data;
+      }
+    }).done(callback);
+  };
+
+  repos.with = function(attr) {
+    return repos.all.filter(function(repo) {
+      return repo[attr];
+    });
+  };
+
+  module.repos = repos;
+})(window);
