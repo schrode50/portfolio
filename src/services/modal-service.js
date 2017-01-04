05c7599983d9954e687a5afe99d498a8769bc7b5
Stephen Schroeder <schrode50@gmail.com>
2017-01-04 10:54:00 -0800

    sorting out services and main components

---
+++ src/services/modal-service.js
@@ -0,0 +1,14 @@
+(function() {
+  angular.module('ModalService', []).factory('modal', [
+    ModalService
+  ]);
+
+  function ModalService() {
+    var modal = {};
+
+    modal.setModal = function(project) {
+      modal.currentProject = project;
+    };
+    return modal;
+  }
+})();
